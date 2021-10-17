fed2962499c18787b87ffb9471157f80003c4ce6
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-17 10:57:15 +0300

    add search button

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  @Output() emitSearch = new EventEmitter<any>()
+  @Output() isRepo = new EventEmitter<any>()
+
+  username = "";
+
+  searchTerm: string;
 
   constructor() { }
-
-  ngOnInit(): void {
+  search(event: any) {
+    this.username = event.target.value
+    this.emitSearch.emit(this.username)
+    console.log(this.username)
+  }
+  ngOnInit() {
   }
 
 }
+
