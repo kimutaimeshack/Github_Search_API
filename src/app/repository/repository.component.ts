e7c738ba74cab40e23ea16fa60a524b9c83cc3b6
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2021-10-17 10:53:21 +0300

    add repository componets

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,15 +1,39 @@
+
+
+
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+import { GitService } from '../git-service.service';
+import { User } from '../user';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+  providers: [GitService],
+  styleUrls: ['./repository.component.css'],
 })
-export class RepositoryComponent implements OnInit {
+export class RepositoriesComponent implements OnInit {
+  Users!: User;
+  Repos: Repository[] = [];
+  constructor(public userHttpService: GitService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.searchGit('kimutaimeshack');
   }
-
+  searchGit(searchTerm: string) {
+    this.userHttpService.searchGits(searchTerm).then(
+      (success) => {
+        this.Users = this.userHttpService.Users;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.userHttpService.searchRepos(searchTerm).then((success) => {
+      this.Repos = this.userHttpService.Repos;
+    });
+  }
 }
+
